d12340668161d9665f6266a122fb39e4149d0d31
Paul Beck <hi@toll-paul.de>
2020-12-28 16:38:24 +0100

    Basic self update components

---
+++ Classes/Utility/UpdateUtility.php
@@ -0,0 +1,16 @@
+<?php
+namespace ERecht24\Er24Rechtstexte\Utility;
+
+class UpdateUtility
+{
+
+    protected $repositoryUrl = 'https://git.muensmedia.de/api/v4/projects/333/branches/master/';
+    protected $repositoryName = '/erecht24/rechtstexte-plugin/erecht24-rechtstexte-plugin-typo3';
+    protected $accessToken = 'oqzuKhxBXES3NDpUGo7Q';
+
+
+    public function __construct() {
+
+    }
+
+}
