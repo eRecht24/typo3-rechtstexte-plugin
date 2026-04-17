1712597fe260cbb1fb8910b11f9d58744308b246
Iven Schlenther <i.schlenther@muensmedia.de>
2026-04-17 13:37:32 +0200

    [TASK] add rector stuff

--- rector.php
+++ rector.php
@@ -9,14 +9,14 @@ return RectorConfig::configure()
         __DIR__ . '/Classes',
         __DIR__ . '/Configuration',
         __DIR__ . '/Resources',
-//        __DIR__ . '/public',
+        __DIR__ . '/public',
     ])
     // uncomment to reach your current PHP version
+    ->withPhpSets(php83: true, php82: true, php84: true, php85: true)
     ->withTypeCoverageLevel(8)
-    ->withSets([
-        \Rector\Set\ValueObject\LevelSetList::UP_TO_PHP_81,
-        \Rector\Set\ValueObject\LevelSetList::UP_TO_PHP_84,
-    ])
+    ->withDeadCodeLevel(0)
+    ->withCodeQualityLevel(0)
     ->withSkip([
         \Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector::class
-    ])->withoutParallel();
+    ])
+    ->withoutParallel();
