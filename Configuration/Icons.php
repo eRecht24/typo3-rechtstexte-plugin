ab256c261ba9c48fc869453cdb9a23f72f2bf93d
Iven Schlenther <i.schlenther@muensmedia.de>
2026-04-20 14:27:42 +0200

    [TASK] Remove unused SVG icon and related icon configuration
    Deleted `Extension14.svg` and removed its reference from `Icons.php` to streamline assets and configuration.

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -7,8 +7,4 @@ return [
         'provider' => SvgIconProvider::class,
         'source' => 'EXT:er24_rechtstexte/Resources/Public/Icons/Extension.svg',
     ],
-    'ext-er24-rechtstexte-module-main' => [
-        'provider' => SvgIconProvider::class,
-        'source' => 'EXT:er24_rechtstexte/Resources/Public/Icons/Extension14.svg',
-    ],
 ];
